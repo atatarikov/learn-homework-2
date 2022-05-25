138e36bb221b1553816e27177af4b83936f08f66
Andrey Tatarikov <atatarikov@gmail.com>
2022-05-25 22:18:59 +0300

    v.2

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -7,13 +7,19 @@
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
+from datetime import datetime, date, timedelta
+
 
 def print_days():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    today = date.today()
+
+    print(today- timedelta(days=1))
+    print(today)
+    print(today- timedelta(days=30))
 
 
 def str_2_datetime(date_string):
@@ -21,7 +27,7 @@ def str_2_datetime(date_string):
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    print(datetime.strptime(date_string, '%d/%m/%y %H:%M:%S.%f'))
 
 if __name__ == "__main__":
     print_days()
--- 2_files.py
+++ 2_files.py
@@ -16,7 +16,13 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    with open('referat.txt', 'r', encoding='utf-8') as f:
+        content = f.read()
+        print(len(content))
+        print(len(content.split(' ')))
+        content = content.replace('.','!')
+        with open('referat2.txt', 'w', encoding='utf-8') as f:
+            f.write(content)
 
 if __name__ == "__main__":
     main()
